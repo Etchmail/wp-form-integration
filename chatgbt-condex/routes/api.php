5d722af55afe934812cb5f2698607969ee74c5a6
Wicket <41268303+TheWicketTK@users.noreply.github.com>
2025-06-25 12:31:19 +0200

    Revert "Add ChatGBT Condex skeleton"

--- chatgbt-condex/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileController;
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/profiles', [ProfileController::class, 'index']);
-    Route::post('/profiles', [ProfileController::class, 'store']);
-});
